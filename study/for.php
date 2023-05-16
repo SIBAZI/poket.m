3a94ad059a4c17ef20e3dc4cf7c6ee5612bc34b5
sibazi <yuzu.taichi.shiba@gmail.com>
2023-05-16 10:18:15 +0900

    少しでも見やすくなるための工夫

---
+++ study/for.php
@@ -0,0 +1,166 @@
+<?php
+
+/* 例 1 */
+
+// for ($i = 1; $i <= 100; $i+=2) {
+//     echo"<br>";
+//     echo $i;
+// }
+
+function sum()
+{
+    $sum = 0;
+    for ($i = 1; $i <= 100; $i++) {
+        $sum += $i;
+    }
+    echo ($sum);
+}
+
+function even()
+{
+    for ($i = 1; $i <= 10; $i++) {
+        echo "<br>";
+        if ($i % 2 === 0) {
+            echo ($i . "は偶数です");
+        } else {
+            echo ($i . "は奇数です");
+        }
+    }
+}
+
+function fizzbuzz($max)
+{
+    for ($i = 1; $i <= $max; $i++) {
+        echo ("<br>");
+        if ($i % 3 === 0 && $i % 5 === 0) {
+            echo ("fizzbuzz");
+        } elseif ($i % 5 === 0) {
+            echo ("buzz");
+        } elseif ($i % 3 === 0) {
+            echo ("fizz");
+        } else {
+            echo ($i);
+        }
+    }
+}
+
+// fizzbuzz(100);
+
+function cc()
+{
+    // echo('<table border="1">');
+    // echo("<tr>");
+    // echo("<td>田中</td>");
+    // echo("<td>27</td>");
+    // echo("</tr>");
+    // echo("<tr>");
+    // echo("<td>佐藤</td>");
+    // echo("<td>42</td>");
+    // echo("</tr>");
+    // echo("</table>");
+
+    echo ('<table border="1">');
+    for ($i = 1; $i <= 9; $i++) {
+        echo ("<tr>");
+        for ($k = 1; $k <= 9; $k++) {
+            # code...
+            $num = $i * $k;
+            echo ("<td>{$num}</td>");
+        }
+        echo ("</tr>");
+    }
+    echo ("</table>");
+
+
+
+}
+
+// cc();
+
+function magicCircle()
+{
+    set_time_limit(1200);
+    for ($n1 = 1; $n1 <= 9; $n1++) {
+        for ($n2 = 1; $n2 <= 9; $n2++) {
+            for ($n3 = 1; $n3 <= 9; $n3++) {
+                for ($n4 = 1; $n4 <= 9; $n4++) {
+                    for ($n5 = 1; $n5 <= 9; $n5++) {
+                        for ($n6 = 1; $n6 <= 9; $n6++) {
+                            for ($n7 = 1; $n7 <= 9; $n7++) {
+                                for ($n8 = 1; $n8 <= 9; $n8++) {
+                                    for ($n9 = 1; $n9 <= 9; $n9++) {
+                                        // 各値の出現回数を数える
+                                        $value_count = array_count_values([$n1, $n2, $n3, $n4, $n5, $n6, $n7, $n8, $n9]);
+                                        // 最大の出現回数を取得する
+                                        if (max($value_count) !== 1)
+                                            continue;
+                                        if (
+                                            $n1 + $n2 + $n3 === 15
+                                            && $n4 + $n5 + $n6 === 15
+                                            && $n7 + $n8 + $n9 === 15
+                                            && $n1 + $n4 + $n7 === 15
+                                            && $n2 + $n5 + $n8 === 15
+                                            && $n3 + $n6 + $n9 === 15
+                                            && $n1 + $n5 + $n9 === 15
+                                            && $n3 + $n5 + $n7 === 15
+                                        ) {
+                                            echo "
+                                            <table border=\"1\">
+                                                <tr>
+                                                    <td>{$n1}</td>
+                                                    <td>{$n2}</td>
+                                                    <td>{$n3}</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>{$n4}</td>
+                                                    <td>{$n5}</td>
+                                                    <td>{$n6}</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>{$n7}</td>
+                                                    <td>{$n8}</td>
+                                                    <td>{$n9}</td>
+                                                </tr>
+                                            </table>
+                                            ";
+                                            exit();
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+// magicCircle();
+
+function primenumber()
+{
+    for ($i = 2; $i <= 100; $i++) {
+        // 初期化
+        $isDivisible =false;
+        for ($k = 2; $k < $i; $k++) {
+            if ($i % $k === 0) {
+                $isDivisible = true;
+            }
+        }
+        if ($isDivisible === false) {
+            echo("<br>");
+            echo($i);
+        }
+    }
+}
+
+// primenumber();
+
+function oturi(int $totale,int $pay)
+// 
+{
+    # code...
+}
+
+oturi(9000,10000);
